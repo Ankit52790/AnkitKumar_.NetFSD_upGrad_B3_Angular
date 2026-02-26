c6b605393b2eb4fa6d582a4cef4c561e62174c0e
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2026-02-26 18:37:45 +0530

    Create state.js

---
+++ 02.Week-2/08.Day8_JavaScript_ES6/Problem-04/mini-product-search/js/state.js
@@ -0,0 +1,13 @@
+
+
+export const state = {
+    products: [
+        { id: 1, name: "iPhone 15" },
+        { id: 2, name: "Samsung Galaxy S23" },
+        { id: 3, name: "MacBook Pro" },
+        { id: 4, name: "Dell XPS Laptop" },
+        { id: 5, name: "Sony Headphones" },
+        { id: 6, name: "Apple Watch" }
+    ],
+    filteredProducts: []
+};
