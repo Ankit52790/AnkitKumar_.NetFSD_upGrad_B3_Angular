bf5a5119bec6d1400f8dc9643203f32b558100d1
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2026-03-24 18:35:53 +0530

    Create Product.cs

---
+++ 06.Week-6/26.Day26_ADO.NET/ProductManagementApp/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace ProductManagementApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
