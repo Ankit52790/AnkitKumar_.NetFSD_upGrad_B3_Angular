2fab0089e675a798f339a8396eec17180166a00a
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2026-03-28 14:16:55 +0530

    Create Example.cs

---
+++ 06.Week-6/28.Day28_ASP.NET_Core_Intro/MyFirstMvcApp/Models/Example.cs
@@ -0,0 +1,9 @@
+namespace MyFirstMvcApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Marks { get; set; }
+    }
+}
