e633611fe0a7be2ee7d2ec9c8aebfeea336f03f0
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2026-03-30 11:50:45 +0530

    Create Product.cs

---
+++ 06.Week-6/28.Day28_ASP.NET_Core_Intro/MVC Application to process product details /Models/Product.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Product Id is required!")]
+        public int ProductId { get; set; }
+      
+        public string ProductName { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+
+ 
+        public int Stock { get; set; }
+    }
+}
