c1cc47e3d7ee7cb3f0cb9629905cfa1712bb316d
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2026-04-06 23:27:17 +0530

    Create IContactRepository.cs

---
+++ 08.Week-8/35.Day-35_Dapper/ContactManagementSystem/DataAccessLayer/Repositories/IContactRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using DataAccessLayer.Models;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Repositories
+{
+
+    public interface IContactRepository
+    {
+        IEnumerable<ContactInfo> GetAllContacts();
+        ContactInfo GetContactById(int id);
+        void AddContact(ContactInfo contact);
+        void UpdateContact(ContactInfo contact);
+        void DeleteContact(int id);
+
+        IEnumerable<Company> GetCompanies();
+        IEnumerable<Department> GetDepartments();
+    }
+}
