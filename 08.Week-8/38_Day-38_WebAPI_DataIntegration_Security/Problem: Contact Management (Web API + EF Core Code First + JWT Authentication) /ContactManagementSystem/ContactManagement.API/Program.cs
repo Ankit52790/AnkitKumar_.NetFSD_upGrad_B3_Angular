c5795b75f93fb51268b40a0a0f6be84d4727bac8
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2026-04-18 01:12:16 +0530

    Create Program.cs

---
+++ 08.Week-8/38_Day-38_WebAPI_DataIntegration_Security/Problem: Contact Management (Web API + EF Core Code First + JWT Authentication) /ContactManagementSystem/ContactManagement.API/Program.cs
@@ -0,0 +1,50 @@
+using ContactManagement.DAL.DbContext;
+using ContactManagement.DAL.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// DB
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+
+// JWT
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("YourSecretKey123"))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();  
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
