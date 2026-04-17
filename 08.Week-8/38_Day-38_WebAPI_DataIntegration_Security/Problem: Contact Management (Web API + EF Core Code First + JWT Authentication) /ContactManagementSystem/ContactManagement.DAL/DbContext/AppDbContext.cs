12013dd8cd4578fc124a5e3e17262fd32ac525c6
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2026-04-18 01:14:26 +0530

    Add files via upload

---
+++ 08.Week-8/38_Day-38_WebAPI_DataIntegration_Security/Problem: Contact Management (Web API + EF Core Code First + JWT Authentication) /ContactManagementSystem/ContactManagement.DAL/DbContext/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactManagement.DAL.DbContext
+{
+    using ContactManagement.DAL.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ContactInfo> Contacts { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //  Define Primary Key FIRST
+            modelBuilder.Entity<ContactInfo>()
+                .HasKey(c => c.ContactId);
+
+            //  Company Relationship
+            modelBuilder.Entity<ContactInfo>()
+                .HasOne(c => c.Company)
+                .WithMany(c => c.Contacts)
+                .HasForeignKey(c => c.CompanyId);
+
+            //  Department Relationship
+            modelBuilder.Entity<ContactInfo>()
+                .HasOne(c => c.Department)
+                .WithMany(d => d.Contacts)
+                .HasForeignKey(c => c.DepartmentId);
+        }
+    }
+}
---
+++ 08.Week-8/38_Day-38_WebAPI_DataIntegration_Security/Problem: Contact Management (Web API + EF Core Code First + JWT Authentication) /ContactManagementSystem/ContactManagement.DAL/Repository/IContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using ContactManagement.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactManagement.DAL.Repository
+{
+    public interface IContactRepository
+    {
+        Task<IEnumerable<ContactInfo>> GetAllAsync();
+        Task<ContactInfo> GetByIdAsync(int id);
+        Task AddAsync(ContactInfo contact);
+        Task UpdateAsync(ContactInfo contact);
+        Task DeleteAsync(int id);
+    }
+}
