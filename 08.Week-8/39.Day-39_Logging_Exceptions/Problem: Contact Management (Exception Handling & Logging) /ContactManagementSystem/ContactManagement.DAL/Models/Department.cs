b0aae5dae9ee571ac657b35ac119a1a7babe3643
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2026-04-18 01:44:53 +0530

    Add files via upload

---
+++ 08.Week-8/39.Day-39_Logging_Exceptions/Problem: Contact Management (Exception Handling & Logging) /ContactManagementSystem/ContactManagement.DAL/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ContactManagement.DAL.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+        public ICollection<ContactInfo> Contacts { get; set; }
+    }
+}
---
+++ 08.Week-8/39.Day-39_Logging_Exceptions/Problem: Contact Management (Exception Handling & Logging) /ContactManagementSystem/ContactManagement.DAL/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactManagement.DAL.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
