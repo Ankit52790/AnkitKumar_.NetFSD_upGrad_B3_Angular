646db8255f44abca2ef070977a769cef01401ad4
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2026-04-20 16:17:33 +0530

    Add files via upload

---
+++ 09-Week-9/40.Day40_WebAPI_Advanced/03.HandsOns_Authentication_EF_Core/ContactManagement.DAL/Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ContactManagement.DAL.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+
+        public ICollection<ContactInfo> Contacts { get; set; }
+    }
+}
---
+++ 09-Week-9/40.Day40_WebAPI_Advanced/03.HandsOns_Authentication_EF_Core/ContactManagement.DAL/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactManagement.DAL.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
